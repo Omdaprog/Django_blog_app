0a6d686685f94a62e01f037228d0de95621435e8
Imed ben kalia <imedrugby2306@gmail.com>
2018-12-13 10:41:08 +0100

    Add feature to make drafts adn publish them.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('',views.post_list, name ='post_list'),
     path('post<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
+    path('post/edit<int:pk>/', views.post_Edit, name='post_edit'),
+    path('post/draft/', views.post_draft_list, name='post_draft_list' ),
+    path('post/publish<int:pk>/',views.post_publish, name='post_publish'),
 ]

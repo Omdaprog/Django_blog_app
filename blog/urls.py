afb8403e739dd0d5ae47a14898e436d2354f6568
Imed ben kalia <imedrugby2306@gmail.com>
2018-12-19 15:42:42 +0100

    Add ability to delete blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('comment/<int:pk>/remove/',views.remove_comment, name="delete"),
 
     path('comment/<int:pk>/approve/',views.comment_approve, name="approve_comment"),
+
+    path('post/<int:pk>/remove', views.remove_post, name="delete_post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -78,6 +78,15 @@ def remove_comment(request, pk):
     return redirect('post_detail', pk=comment.post.pk)
 
 @login_required
+def remove_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('/')
+
+
+
+
+@login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
